0e456493d72255015f2686e53e447a1f3ff5e11c
Tan Wei Liang [WeiLightbook] <tan_wei_liang@myrp.edu.sg>
2026-04-27 22:49:00 +0800

    L03 Tutorial, Slide 22 Add Variables

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+	public string playerName = "Nova";
+	public int health = 100;
+	public float rotationSpeed = 0.0f;
+	public float size = 1.0f;
+	public bool aliveState = true;
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
