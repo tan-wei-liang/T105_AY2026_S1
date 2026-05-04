89d99bfded1bd3c604c3d67321c0aa9927d39d91
Tan Wei Liang [WeiLightbook] <tan_wei_liang@myrp.edu.sg>
2026-05-04 18:00:14 +0800

    L05 Tutorial, Slide 22 Step 8: Add Hold Logic -Added code that allows player to jump higher if Space button is held down and player is still moving upwards

--- Assets/Scripts/FrogController.cs
+++ Assets/Scripts/FrogController.cs
@@ -25,5 +25,21 @@ public class FrogController : MonoBehaviour
             // and simulating jumps
 			rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 		}
+		
+		// Condition will be true on the frame that space
+		// button was pressed and true for the following
+		// frames that the button is held IF rigidbody is
+		// still moving upwards
+		if (Input.GetKey(KeyCode.Space) && rb.linearVelocity.y > 0.0f)
+		{
+			// Compares current linearVelocity of rigidbody with
+			// class variable, maxUpwardVelocity
+			if (rb.linearVelocity.y < maxUpwardVelocity)
+			{
+				// Continue apply force so that player jumps higher while
+				// Space button is held down
+				rb.AddForce(Vector2.up * holdForce, ForceMode2D.Force);
+			}
+		}
 	}
 }
