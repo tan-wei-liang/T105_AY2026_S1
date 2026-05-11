540a0b54afeca81c72b89f013e8a524b120d137c
Tan Wei Liang [WeiLightbook] <tan_wei_liang@myrp.edu.sg>
2026-05-11 17:29:02 +0800

    L08 Instructor Demo, Slide 10 -Added player choice by comparing tag    -Rock == 0    -Paper == 1    -Scissors == 2

--- Assets/Scripts/PlayerChoice.cs
+++ Assets/Scripts/PlayerChoice.cs
@@ -6,7 +6,19 @@ public class PlayerChoice : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // TODO: Detect Rock / Paper / Scissors
+		// TODO: Detect Rock / Paper / Scissors
+		if (other.CompareTag("Rock"))
+		{
+			playerMove = 0;
+		}
+		else if (other.CompareTag("Paper"))
+		{
+			playerMove = 1;
+		}
+		else if (other.CompareTag("Scissors"))
+		{
+			playerMove = 2;
+		}
 
 		// TODO: Generate AI choice using Random.Range()
 
