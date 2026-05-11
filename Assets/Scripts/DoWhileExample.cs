789c4fe6cdbaf7338e41a8554fe89e4fb554f205
Tan Wei Liang [WeiLightbook] <tan_wei_liang@myrp.edu.sg>
2026-05-11 15:49:06 +0800

    L07 Do-While Example Added DoWhileExample.cs Added DoWhileExample under Loop_Examples in MovementRecap scene -Upon playing, Triangle will scale to [3, 3, 3] -Upon pressing 'E', Triangle will scale to [-1. -1, -1] and return back to [3, 3, 3] after 2s Modify Scaled Size, Initial Size, Scale Speed and Coroutine Interval to test how Do-While loop works

---
+++ Assets/Scripts/DoWhileExample.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class DoWhileExample : MonoBehaviour
+{
+	[SerializeField] 
+    Transform transformToScale  = null;
+    [SerializeField]
+    float scaledSize            = 2.0f;
+    [SerializeField]
+    float initialSize           = 1.0f;
+    [SerializeField]
+    float scaleSpeed            = -0.05f;
+    [SerializeField]
+	KeyCode activationKey       = KeyCode.E;
+    [SerializeField]
+    float coroutineInterval     = 0.01f;
+    [SerializeField]
+    TMP_Text doWhileText        = null;
+
+    string coroutineName        = "DoWhileCoroutine";
+    bool isCoroutineRunning     = false;
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		// Ensure that scaledSize, initialSize are positive
+		scaledSize                      = Mathf.Abs(scaledSize);
+		initialSize                     = Mathf.Abs(initialSize);
+		// Ensure that scaledSize is bigger than initialSize,
+		// Otherwise flip the values
+		//if (scaledSize < initialSize)
+  //      {
+  //          float tempSize            = scaledSize;
+  //          scaledSize                = initialSize;
+  //          initialSize               = tempSize;
+  //      }
+        // Ensure that scaleSpeed is negative
+		scaleSpeed                      = -Mathf.Abs(scaleSpeed);
+		// Ensure that transform is set to initialSize
+		transformToScale.localScale     = Vector3.one * initialSize;
+    }
+
+	// Update is called once per frame
+	void Update()
+    {
+        // Start coroutine if activationKey is pressed and
+        // transform has returned to initialSize
+        if(Input.GetKey(activationKey) && transformToScale.localScale.x == initialSize)
+        {
+            StartCoroutine(coroutineName);
+            isCoroutineRunning  = true;
+		}
+
+        // Transform is still scaling down
+        if(isCoroutineRunning)
+        {
+            return;
+        }
+
+        // Transform has already been reset to initialSize
+        if(transformToScale.localScale.x == initialSize)
+        {
+            return;
+        }
+
+		// Reset transform to initialSize
+		transformToScale.localScale = Vector3.one * initialSize;
+        UpdateDoWhileText();
+	}
+
+    IEnumerator DoWhileCoroutine()
+	{
+		transformToScale.localScale         = Vector3.one * scaledSize;
+        do
+        {
+            Vector3 newSize                 = transformToScale.localScale + Vector3.one * scaleSpeed * coroutineInterval;
+			transformToScale.localScale     = newSize;
+            UpdateDoWhileText();
+			yield return new WaitForSeconds(coroutineInterval);
+        }
+        while (transformToScale.localScale.x > initialSize);
+        isCoroutineRunning = false;
+	}
+
+    void UpdateDoWhileText()
+	{
+		float truncatedScale = (int)(transformToScale.localScale.x * 100) / 100.0f;
+		doWhileText.text = $"Object scale: {truncatedScale}";
+	}
+}
