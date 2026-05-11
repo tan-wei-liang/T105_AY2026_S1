75d31cd1809fff0459be1838fab39da0ec3f4884
Tan Wei Liang [WeiLightbook] <tan_wei_liang@myrp.edu.sg>
2026-05-11 17:42:45 +0800

    L08 Instructor Demo, Slide 13 Added Loops/Rounds Added debug log to print number of times player win, lose and draw

--- Assets/Scripts/PlayerChoice.cs
+++ Assets/Scripts/PlayerChoice.cs
@@ -1,10 +1,18 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using static Unity.Collections.Unicode;
 
 public class PlayerChoice : MonoBehaviour
 {
-    public int playerMove = -1;
+    public int playerMove	= -1;
+	public int round		= 0;
+	public int maxRounds	= 3;
 
-    private void OnTriggerEnter2D(Collider2D other)
+	public int win	= 0;
+	public int lose = 0;
+	public int draw = 0;
+
+	private void OnTriggerEnter2D(Collider2D other)
     {
 		// TODO: Detect Rock / Paper / Scissors
 		if (other.CompareTag("Rock"))
@@ -28,6 +36,7 @@ public class PlayerChoice : MonoBehaviour
 		if (playerMove == aiMove)
 		{
 			Debug.Log("Draw");
+			draw++;
 		}
 		// Player: Rock (0),		AI: Paper (1),		Difference = -1
 		// Player: Paper (1),		AI: Scissors (2),	Difference = -1
@@ -35,6 +44,7 @@ public class PlayerChoice : MonoBehaviour
 		else if (playerMove - aiMove == -1 || playerMove - aiMove == 2)
 		{
 			Debug.Log("Win");
+			win++;
 		}
 		// Player: Rock (0),		AI: Scissors (2),	Difference = -2
 		// Player: Paper (1),		AI: Rock (0),		Difference = 1
@@ -42,10 +52,17 @@ public class PlayerChoice : MonoBehaviour
 		else
 		{
 			Debug.Log("Lose");
+			lose++;
 		}
 
 		// TODO: Update round
+		round++;
 
 		// TODO: Display result using Debug.Log()
+		if (round >= maxRounds)
+		{
+			Debug.Log("Game Over!");
+			Debug.Log($"Results: {win} Win(s), {lose} Lose(s), {draw} Draw(s) ");
+		}
 	}
 }
