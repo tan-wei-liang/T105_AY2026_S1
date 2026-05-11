0bf0ea53231d4e395bd250d548f027238d965d4c
Tan Wei Liang [WeiLightbook] <tan_wei_liang@myrp.edu.sg>
2026-05-11 17:18:04 +0800

    L07 Tutorial, Slide 34-39 Step 6: Add the Enemy Prefab Variable Step 7: Detect the SPACE Key Step 8: Add a Spawn Method Step 9: Add the For Loop Step 10: Create a Position for Each Enemy Step 11: Spawn the Enemy

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,7 +1,12 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public GameObject enemyPrefab   = null;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -10,7 +15,25 @@ public class EnemySpawner : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+	{
+		//Step 7: Detect the SPACE Key
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+			//Step 8: Add a Spawn Method, call the method
+			SpawnEnemies();
+        }
     }
+
+	//Step 8: Add a Spawn Method, create the method
+	void SpawnEnemies()
+    {
+		//Step 9: Add the For Loop
+		for (int i = 0; i < 3; i++)
+        {
+			//Step 10: Create a Position for Each Enemy
+			Vector3 position = new Vector3(i * 2, 0, 0);
+			//Step 11: Spawn the Enemy
+			Instantiate(enemyPrefab, position, Quaternion.identity);
+		}
+	}
 }
