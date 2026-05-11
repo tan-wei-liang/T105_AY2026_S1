1ec2891fb19b54fa75105a362eb4dc52fcd2d4c2
Tan Wei Liang [WeiLightbook] <tan_wei_liang@myrp.edu.sg>
2026-05-11 13:15:13 +0800

    L07 Recap Activity Modified PlayerMovement.cs -Encapsulated movement code into ProcessKeys_WASD() -Added function, ProcessAxes_Movement(), to read player input using Input.GetAxis() -Added conditional statement to switch between ProcessKeys_WASD() and ProcessAxes_Movement()

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,20 +2,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed	= 5.0f;
+	public bool isUsingAxes = false;
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
-        }
+		if (isUsingAxes)
+		{
+			ProcessKeys_WASD();
+		}
+		else
+		{
+			ProcessAxes_Movement();
+		}
+	}
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-        }
+	void ProcessAxes_Movement()
+	{
+		Vector2 movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		transform.Translate(movementInput * moveSpeed * Time.deltaTime);
+	}
 
+	void ProcessKeys_WASD()
+	{
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+		}
 		// TODO: Add movement for W
 		if (Input.GetKey(KeyCode.W))
 		{
