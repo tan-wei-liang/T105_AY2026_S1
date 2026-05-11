0ab77befce8abcde032870ff193d29ebee79c13a
Tan Wei Liang [WeiLightbook] <tan_wei_liang@myrp.edu.sg>
2026-05-11 14:46:50 +0800

    L07 While Loop Example -Modified WhileExample.cs    -Encapsulated previous code in Update() into NonCoroutineUpdate()    -Added coroutine to show how while loop works (with visuals)    -Modified Update() to run NonCoroutineUpdate() or CoroutineUpdate(), depending on bool, isUsingCoroutine

--- Assets/Scripts/WhileExample.cs
+++ Assets/Scripts/WhileExample.cs
@@ -1,63 +1,121 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
 public class WhileExample : MonoBehaviour
 {
     [SerializeField]
-    TMP_Text whileText  = null;
+    TMP_Text whileText      = null;
     [SerializeField]
-    string tagToCompare = "Player";
-    float timer         = 0.0f;
-    bool isInBoundaries = false;
+    string tagToCompare     = "Player";
+
+    [SerializeField]
+    bool isUsingCoroutine   = false;
+    bool isCoroutineStarted = false;
+	[SerializeField]
+	float coroutineInterval = 0.05f;
+	string coroutineName	= "CoroutineUpdate";
+
+	float timer             = 0.0f;
+    bool isInBoundaries     = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (isUsingCoroutine)
+        {
+            StartCoroutine(coroutineName);
+        }
     }
 
     // Update is called once per frame
     void Update()
-    {
-        // Player is not in boundaries, no need to update
-        if(!isInBoundaries)
-        {
-            return;
-        }
+	{
+		if (isUsingCoroutine)
+		{
+			if (isCoroutineStarted)
+			{
+				return;
+			}
+			// Start coroutine if it has not started
+			StartCoroutine(coroutineName);
+			isCoroutineStarted	= true;
+		}
+		else
+		{
+			NonCoroutineUpdate();
 
-        // update time player has spent in boundaries
-        timer += Time.deltaTime;
-
-        // whileText is not set up, no need to display
-        if(whileText == null)
-        {
-            return;
-        }
-
-        // (int) will cast the float into int, thus
-        // removing any numbers behind the decimal point
-        // Casting is an act of converting data from one
-        // type to another, may crash the program is the
-        // resulting data type is not suitable.
-        // Casting is used here to limit the precision of 
-        // the float displayed.
-        float truncatedTimer = (int)(timer * 100) / 100.0f ;
-		whileText.text  = $"While Timer: {truncatedTimer}";
+			if (!isCoroutineStarted)
+			{
+				return;
+			}
+			// Stop coroutine if it has not stopped
+			StopCoroutine(coroutineName);
+			isCoroutineStarted = false;
+		}
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		if(collision.CompareTag(tagToCompare))
-        {
-            isInBoundaries = true;
-        }
+		if (collision.CompareTag(tagToCompare))
+		{
+			isInBoundaries = true;
+		}
 	}
 
 	private void OnTriggerExit2D(Collider2D collision)
 	{
 		if (collision.CompareTag(tagToCompare))
 		{
-			isInBoundaries = false;
+			isInBoundaries	= false;
 		}
+	}
+
+    void NonCoroutineUpdate()
+	{
+		// Player is not in boundaries, no need to update
+		if (!isInBoundaries)
+		{
+			return;
+		}
+
+		// update time player has spent in boundaries
+		timer	+= Time.deltaTime;
+
+		// whileText is not set up, no need to display
+		if (whileText == null)
+		{
+			return;
+		}
+
+		UpdateWhileText();
+		Debug.Log("Update in NonCoroutineUpdate()");
+	}
+
+	IEnumerator CoroutineUpdate()
+    {
+        while(true)
+        {
+			if(isInBoundaries)
+			{
+				timer += coroutineInterval;
+				UpdateWhileText();
+				Debug.Log("Update in CoroutineUpdate()");
+			}
+			yield return new WaitForSeconds(coroutineInterval);
+		}
+    }
+
+	void UpdateWhileText()
+	{
+		// (int) will cast the float into int, thus
+		// removing any numbers behind the decimal point
+		// Casting is an act of converting data from one
+		// type to another, may crash the program is the
+		// resulting data type is not suitable.
+		// Casting is used here to limit the precision of 
+		// the float displayed.
+		float truncatedTimer	= (int)(timer * 100) / 100.0f;
+		whileText.text			= $"While Timer: {truncatedTimer}";
 	}
 }
