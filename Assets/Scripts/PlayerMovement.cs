203b6920a03c5ada80ea5606ab0d2e7f0149ea4c
Tan Wei Liang [WeiLightbook] <tan_wei_liang@myrp.edu.sg>
2026-05-12 18:52:48 +0800

    L08 CA1 Dry Run - The Fun Stuff Replaced shapes representing Rock, Paper, Scissors with iconic sprites Replaced shape represeting Player with player sprite used in L07 Added code to flip player when moving left or right Moved game objects in scene so that it is easier to move select Rock, Paper or Scissors

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed  = 5f;
+    SpriteRenderer sr       = null;
+
+    void Start()
+    {
+        sr  = GetComponent<SpriteRenderer>();
+    }
+
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+            sr.flipX    = true;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+            sr.flipX    = false;
         }
 
         if (Input.GetKey(KeyCode.W))
