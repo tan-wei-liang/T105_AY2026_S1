da5b8c8f0e71eeb4d9c6ca10b40c79584f50f950
Tan Wei Liang [WeiLightbook] <tan_wei_liang@myrp.edu.sg>
2026-05-12 17:45:24 +0800

    L08 Instructor Demo, Slide 12 Added outcome logic

--- Assets/Scripts/PlayerChoice.cs
+++ Assets/Scripts/PlayerChoice.cs
@@ -24,7 +24,32 @@ public class PlayerChoice : MonoBehaviour
 		int aiMove = Random.Range(0, 3);
 		Debug.Log(aiMove);
 
+		// Difference between playerMove and aiMove can be used to 
+		// determine result of each round
+		int difference	= playerMove - aiMove;
+
 		// TODO: Compare player vs AI (Win / Lose / Draw)
+		// Player: Rock (0),		AI: Rock (0),		Difference = 0
+		// Player: Paper (1),		AI: Paper (1),		Difference = 0
+		// Player: Scissors (2),	AI: Scissors (2),	Difference = 0
+		if (difference == 0)
+		{
+			Debug.Log("Draw");
+		}
+		// Player: Rock (0),		AI: Scissors (2),	Difference = -2
+		// Player: Paper (1),		AI: Rock (0),		Difference = 1
+		// Player: Scissors (2),	AI: Paper (1),		Difference = 1
+		else if (difference == 1 || difference == -2)
+		{
+			Debug.Log("Win");
+		}
+		// Player: Rock (0),		AI: Paper (1),		Difference = -1
+		// Player: Paper (1),		AI: Scissors (2),	Difference = -1
+		// Player: Scissors (2),	AI: Rock (0),		Difference = 2
+		else
+		{
+			Debug.Log("Lose");
+		}
 
 		// TODO: Update round
 
