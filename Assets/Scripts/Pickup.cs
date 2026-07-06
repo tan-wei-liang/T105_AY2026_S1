9e527ac111c386794656596fe25039212b4360e5
Tan Wei Liang <tanweiliang1988@hotmail.com>
2026-07-06 14:53:38 +0800

    L18 Tutorial, Slide 14 Pickup – OnTriggerEnter2D() -Modified PickUp.cs

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -14,16 +14,24 @@ public class Pickup : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-        //TODO: check if the colliding object has a tag of "Player"
-        
-        //TODO: loop through the inventory slots using a for loop
-            
-        //TODO: check if the current slot is not full
-                
-        //TODO: mark the slot as full
-
-         //TODO: break out of the loop after filling one slot
-                   
-    }
+		//TODO: check if the colliding object has a tag of "Player"
+		if (collision.CompareTag("Player"))
+		{
+			//TODO: loop through the inventory slots using a for loop
+			for (int i = 0; i < inventory.isFull.Length; i++)
+			{
+				//TODO: check if the current slot is not full
+				if (inventory.isFull[i] == false)
+				{
+					//TODO: mark the slot as full
+					inventory.isFull[i] = true;
+					//instantiate the button in the current slot and remove pickup from scene
+					Instantiate(itemButton, inventory.slots[i].transform);
+					Destroy(gameObject);
+					//TODO: break out of the loop after filling one slot
+					break;
+				}
+			}
+		}
+	}
 }
