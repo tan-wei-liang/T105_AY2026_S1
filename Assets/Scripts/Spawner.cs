89350558d211a4258d8b674ca478075ac0cea382
Tan Wei Liang <tanweiliang1988@hotmail.com>
2026-07-06 14:32:00 +0800

    L17 Tutorial, Slide 19-21 Step 13: Use InvokeRepeating Step 15: Spawn Zombies with Random Positions Step 14: Stop Spawning with CancelInvoke -Modified Start() in Spawner.cs to use InvokeRepeating -Added SpawnZombie() -SpawnZombie() checks boolean, stopSpawning, to CancelInvoke -Added zombie prefab to as zombie prefab

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -13,12 +13,30 @@ public class Spawner : MonoBehaviour
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
     {
-        
-    }
+		// Step 13, works the same as code shown in slides
+		InvokeRepeating("SpawnZombie", spawnTime, spawnDelay);
+	}
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+	public void SpawnZombie()
+	{
+		// random horizontal position
+		float randomX = Random.Range(-3.3f, 3.3f);
+		//create a spawn position
+		Vector3 spawnpos = new Vector3(randomX, spawnY, 0f);
+
+		//spawn zombie
+		Instantiate(zombiePrefab, spawnpos, Quaternion.identity);
+
+		//if stop spawning is true, stop InvokeRepeating() from calling SpawnZombie() again
+		if (stopSpawning)
+		{
+			CancelInvoke("SpawnZombie");
+		}
+	}
 }
