d66bf979731fdbf2d86023fbd9145575e8774e50
Tan Wei Liang <tanweiliang1988@hotmail.com>
2026-07-06 14:08:19 +0800

    L17 Tutorial, Slide 10 Step 4: Move the Zombie Downwards -Added code from slide 10 into ZombieMovement.cs

--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class ZombieMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+	public float moveSpeed = 5f;
+	public float lowestY;
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
     {
         
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
+	{
+		//move the zombie downwards over time
+		transform.Translate(0, -moveSpeed * Time.deltaTime, 0);
+		//destroy the zombie if it goes below the screen
+		if (transform.position.y < lowestY)
+		{
+			Destroy(gameObject);
+		}
+	}
 }
