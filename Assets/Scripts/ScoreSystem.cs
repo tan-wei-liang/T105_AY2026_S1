f8a685d5e954001865c8f5a07225c2c643ca0fad
Tan Wei Liang <tanweiliang1988@hotmail.com>
2026-07-06 14:43:00 +0800

    L17 Tutorial, Slide 28-29 Step 21: Declare a Static Score Variable Step 22: Display the Score in the Text UI -Added code as per slides

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -1,9 +1,14 @@
+using TMPro;
 using UnityEngine;
 
 public class ScoreSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+	public TMP_Text scoreTxt    = null;
+	public static int score     = 0;
+
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
     {
         
     }
@@ -11,6 +16,7 @@ public class ScoreSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		// Update the UI text every frame
+		scoreTxt.text = "Score: " + score;
+	}
 }
