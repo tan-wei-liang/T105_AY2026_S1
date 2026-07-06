fa4a5f26f580eda9974e9df53d54a32c06b8ccf1
Tan Wei Liang <tanweiliang1988@hotmail.com>
2026-07-06 14:21:59 +0800

    L17 Tutorial, Slide 18 Step 12: Add Variables to the Spawner -Added Spawner.cs -Added Spawner.cs as component to SpawnPoint game object -Added variables as shown in Slide 18 to Spawner.cs

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject zombiePrefab  = null;
+	public float spawnTime          = 1f;
+	public float spawnDelay         = 1f;
+	public bool stopSpawning        = false;
+
+	private float spawnY            = 5.8f;
+
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
