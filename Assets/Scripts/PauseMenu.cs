a8f7af3b9b61e88729e742313e05c73f904d109b
Tan Wei Liang [WeiLightbook] <tan_wei_liang@myrp.edu.sg>
2026-07-21 08:08:42 +0800

    L21 Tutorial, Slide 31 Step 5: Add References and Input Check Generated ResumeGame() and PauseGame() functions but yet to define them

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,7 +1,11 @@
+using System;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject pausePanel    = null;
+    bool isPaused                   = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,23 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
     }
+
+	private void PauseGame()
+	{
+		throw new NotImplementedException();
+	}
+
+	private void ResumeGame()
+	{
+		throw new NotImplementedException();
+	}
 }
